9b0797099f48c0510ae1aac250bd06a0b83d3597
ksx919 <1983262652@qq.com>
2025-09-17 20:28:31 +0800

    新增member会员模块

--- train-member/src/main/java/com/fanxin/train/member/controller/TestController.java
+++ train-member/src/main/java/com/fanxin/train/member/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.fanxin.train;
+package com.fanxin.train.member.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/hello")
     public String hello(){
-        return "Hello World!";
+        return "Hello World!%";
     }
 }
