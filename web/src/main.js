f5820b3a2b6895213a7652ede90f4f35604a9c0b
ksx919 <1983262652@qq.com>
2025-09-18 20:59:32 +0800

    增加axios拦截器，打印请求参数和返回结果

--- web/src/main.js
+++ web/src/main.js
@@ -5,6 +5,25 @@ import store from './store'
 import 'ant-design-vue/dist/antd.css'
 import Antd from 'ant-design-vue'
 import * as Icons from '@ant-design/icons-vue'
+import axios from 'axios';
+
+/**
+ * axios拦截器
+ */
+axios.interceptors.request.use(function (config) {
+    console.log('请求参数：', config);
+    return config;
+}, error => {
+    return Promise.reject(error);
+});
+axios.interceptors.response.use(function (response) {
+    console.log('返回结果：', response);
+    return response;
+}, error => {
+    console.log('返回错误：', error);
+    return Promise.reject(error);
+});
+
 
 const app = createApp(App);
 app.use(Antd).use(store).use(router).mount('#app');
--- web/src/views/LoginPage.vue
+++ web/src/views/LoginPage.vue
@@ -31,7 +31,7 @@
         </a-form-item>
 
         <a-form-item>
-          <a-button type="primary" block html-type="submit">登录</a-button>
+          <a-button type="primary" block @click="login">登录</a-button>
         </a-form-item>
 
       </a-form>
@@ -40,7 +40,9 @@
 </template>
 
 <script>
+import axios from 'axios';
 import { defineComponent, reactive } from 'vue';
+import { notification } from 'ant-design-vue';
 export default defineComponent({
   name: "login-view",
   setup() {
@@ -48,16 +50,36 @@ export default defineComponent({
       mobile: '13000000000',
       code: '',
     });
-    const onFinish = values => {
-      console.log('Success:', values);
+    const sendCode = () => {
+      axios.post("http://localhost:8000/member/member/send-code", {
+        mobile: loginForm.mobile
+      }).then(response => {
+        console.log(response);
+        let data = response.data;
+        if (data.success) {
+          notification.success({ description: '发送验证码成功！' });
+          loginForm.code = "8888";
+        } else {
+          notification.error({ description: data.message });
+        }
+      });
     };
-    const onFinishFailed = errorInfo => {
-      console.log('Failed:', errorInfo);
+
+    const login = () => {
+      axios.post("http://localhost:8000/member/member/login", loginForm).then(response => {
+        let data = response.data;
+        if (data.success) {
+          notification.success({ description: '登录成功！' });
+          console.log("登录成功：", data.content);
+        } else {
+          notification.error({ description: data.message });
+        }
+      })
     };
     return {
       loginForm,
-      onFinish,
-      onFinishFailed,
+      sendCode,
+      login
     };
   },
 });
