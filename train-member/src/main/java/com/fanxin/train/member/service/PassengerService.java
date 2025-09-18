80357213deefc4c443deaee6ede324b95a190867
ksx919 <1983262652@qq.com>
2025-09-18 23:00:51 +0800

    增加乘车人新增接口

---
+++ train-member/src/main/java/com/fanxin/train/member/service/PassengerService.java
@@ -0,0 +1,10 @@
+package com.fanxin.train.member.service;
+
+import com.fanxin.train.member.req.PassengerSaveReq;
+
+public interface PassengerService {
+
+    void save(PassengerSaveReq req);
+
+
+}
---
+++ train-member/src/main/java/com/fanxin/train/member/service/impl/PassengerServiceImpl.java
@@ -0,0 +1,27 @@
+package com.fanxin.train.member.service.impl;
+
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.date.DateTime;
+import com.fanxin.train.common.util.SnowUtil;
+import com.fanxin.train.member.domain.Passenger;
+import com.fanxin.train.member.mapper.PassengerMapper;
+import com.fanxin.train.member.req.PassengerSaveReq;
+import com.fanxin.train.member.service.PassengerService;
+import jakarta.annotation.Resource;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PassengerServiceImpl implements PassengerService {
+    @Resource
+    private PassengerMapper passengerMapper;
+
+    @Override
+    public void save(PassengerSaveReq req) {
+        DateTime now = DateTime.now();
+        Passenger passenger = BeanUtil.copyProperties(req, Passenger.class);
+        passenger.setId(SnowUtil.getSnowflakeNextId());
+        passenger.setCreateTime(now);
+        passenger.setUpdateTime(now);
+        passengerMapper.insert(passenger);
+    }
+}
