9a70f9ff5b9ae47f9a967ed6cfe3057584b0ce44
ksx919 <1983262652@qq.com>
2025-09-20 12:21:02 +0800

    解决LONG类型前端精度丢失问题

--- train-member/src/main/java/com/fanxin/train/member/resp/PassengerQueryResp.java
+++ train-member/src/main/java/com/fanxin/train/member/resp/PassengerQueryResp.java
@@ -1,14 +1,19 @@
 package com.fanxin.train.member.resp;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import lombok.Data;
 
 import java.util.Date;
 
 @Data
 public class PassengerQueryResp {
+
+    @JsonSerialize(using= ToStringSerializer.class)
     private Long id;
 
+    @JsonSerialize(using= ToStringSerializer.class)
     private Long memberId;
 
     private String name;
@@ -22,4 +27,5 @@ public class PassengerQueryResp {
 
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date updateTime;
+
 }
