ba32357a25ff4a067e777fe90637e235cc92d557
ksx919 <1983262652@qq.com>
2025-09-20 11:15:14 +0800

    使用线程本地变量存储会员信息

--- train-member/src/main/java/com/fanxin/train/member/req/PassengerSaveReq.java
+++ train-member/src/main/java/com/fanxin/train/member/req/PassengerSaveReq.java
@@ -1,7 +1,6 @@
 package com.fanxin.train.member.req;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.util.Date;
@@ -10,7 +9,6 @@ import java.util.Date;
 public class PassengerSaveReq {
     private Long id;
 
-    @NotNull(message = "【会员ID】不能为空")
     private Long memberId;
 
     @NotBlank(message = "【名字】不能为空")
@@ -28,18 +26,16 @@ public class PassengerSaveReq {
 
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(getClass().getSimpleName());
-        sb.append(" [");
-        sb.append("Hash = ").append(hashCode());
-        sb.append(", id=").append(id);
-        sb.append(", memberId=").append(memberId);
-        sb.append(", name=").append(name);
-        sb.append(", idCard=").append(idCard);
-        sb.append(", type=").append(type);
-        sb.append(", createTime=").append(createTime);
-        sb.append(", updateTime=").append(updateTime);
-        sb.append("]");
-        return sb.toString();
+        return getClass().getSimpleName() +
+                " [" +
+                "Hash = " + hashCode() +
+                ", id=" + id +
+                ", memberId=" + memberId +
+                ", name=" + name +
+                ", idCard=" + idCard +
+                ", type=" + type +
+                ", createTime=" + createTime +
+                ", updateTime=" + updateTime +
+                "]";
     }
 }
--- train-member/src/main/java/com/fanxin/train/member/service/impl/PassengerServiceImpl.java
+++ train-member/src/main/java/com/fanxin/train/member/service/impl/PassengerServiceImpl.java
@@ -2,6 +2,7 @@ package com.fanxin.train.member.service.impl;
 
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.date.DateTime;
+import com.fanxin.train.common.context.LoginMemberContext;
 import com.fanxin.train.common.util.SnowUtil;
 import com.fanxin.train.member.domain.Passenger;
 import com.fanxin.train.member.mapper.PassengerMapper;
@@ -20,6 +21,7 @@ public class PassengerServiceImpl implements PassengerService {
         DateTime now = DateTime.now();
         Passenger passenger = BeanUtil.copyProperties(req, Passenger.class);
         passenger.setId(SnowUtil.getSnowflakeNextId());
+        passenger.setMemberId(LoginMemberContext.getId());
         passenger.setCreateTime(now);
         passenger.setUpdateTime(now);
         passengerMapper.insert(passenger);
