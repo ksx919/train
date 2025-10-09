cca32d2e4acf4473de1e46df1e267982f019d029
ksx919 <1983262652@qq.com>
2025-10-09 10:34:42 +0800

    保存确认订单表，状态初始

--- train-business/src/main/java/com/fanxin/train/business/service/impl/ConfirmOrderServiceImpl.java
+++ train-business/src/main/java/com/fanxin/train/business/service/impl/ConfirmOrderServiceImpl.java
@@ -3,7 +3,10 @@ package com.fanxin.train.business.service.impl;
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.date.DateTime;
 import cn.hutool.core.util.ObjectUtil;
+import com.alibaba.fastjson.JSON;
+import com.fanxin.train.business.enums.ConfirmOrderStatusEnum;
 import com.fanxin.train.business.req.ConfirmOrderDoReq;
+import com.fanxin.train.common.context.LoginMemberContext;
 import com.fanxin.train.common.resp.PageResp;
 import com.fanxin.train.common.util.SnowUtil;
 import com.fanxin.train.business.domain.ConfirmOrder;
@@ -78,6 +81,21 @@ public class ConfirmOrderServiceImpl implements ConfirmOrderService {
         //省略业务数据校验，如：车次是否存在，余票是否存在，车次是否在有效期内，ticket条数>0，同乘客同车次是否已经买过
 
         //保存确认订单表，状态初始
+        DateTime now = DateTime.now();
+        ConfirmOrder confirmOrder = new ConfirmOrder();
+        confirmOrder.setId(SnowUtil.getSnowflakeNextId());
+        confirmOrder.setMemberId(LoginMemberContext.getId());
+        confirmOrder.setDate(req.getDate());
+        confirmOrder.setTrainCode(req.getTrainCode());
+        confirmOrder.setStart(req.getStart());
+        confirmOrder.setEnd(req.getEnd());
+        confirmOrder.setDailyTrainTicketId(req.getDailyTrainTicketId());
+        confirmOrder.setStatus(ConfirmOrderStatusEnum.INIT.getCode());
+        confirmOrder.setCreateTime(now);
+        confirmOrder.setUpdateTime(now);
+        confirmOrder.setTickets(JSON.toJSONString(req.getTickets()));
+
+        confirmOrderMapper.insert(confirmOrder);
 
         //查出余票记录，需要得到真实的库存
 
